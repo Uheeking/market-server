d5beae648cabc20fc03ebf19a2541bd19f4549d2
Uheeking <wert1847@naver.com>
2022-05-27 17:42:04 +0900

    server 보완

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const upload = multer({
     },
   }),
 });
+const detectProduct = require("./helpers/detectProduct");
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
@@ -30,15 +31,22 @@ app.get("/banners", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).send("에러 발생");
+      res.status(500).send("에러가 발생했습니다");
     });
 });
 
 app.get("/products", (req, res) => {
   models.Product.findAll({
     order: [["createdAt", "DESC"]],
-    //filter -> 필요한 데이터만 출력할 수 있게 조정하는 구간임
-    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl","soldout"],
+    attributes: [
+      "id",
+      "name",
+      "price",
+      "createdAt",
+      "seller",
+      "imageUrl",
+      "soldout",
+    ],
   })
     .then((result) => {
       console.log("PRODUCTS : ", result);
@@ -58,23 +66,19 @@ app.post("/products", (req, res) => {
   if (!name || !description || !price || !seller || !imageUrl) {
     res.status(400).send("모든 필드를 입력해주세요");
   }
-  models.Product.create({
-    name,
-    description,
-    price,
-    seller,
-    imageUrl,
-  })
-    .then((result) => {
-      console.log("상품생성 결과 : ", result);
-      res.send({
-        result,
+  detectProduct(imageUrl, (type) => {
+    models.Product.create({ description, price, seller, imageUrl, name, type })
+      .then((result) => {
+        console.log("상품 생성 결과 : ", result);
+        res.send({
+          result,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(400).send("상품 업로드에 문제가 발생했습니다");
       });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
-    });
+  });
 });
 
 app.get("/products/:id", (req, res) => {
@@ -86,17 +90,17 @@ app.get("/products/:id", (req, res) => {
     },
   })
     .then((result) => {
-      console.log("PRODUCT :", result);
+      console.log("PRODUCT : ", result);
       res.send({
         product: result,
       });
     })
     .catch((error) => {
       console.error(error);
-      res.status(400).send("에러가 발생했습니다.");
+      res.status(400).send("상품 조회에 에러가 발생했습니다");
     });
 });
-// 이미지 하나만 보낼 때 single로 해서 보냄
+
 app.post("/image", upload.single("image"), (req, res) => {
   const file = req.file;
   console.log(file);
@@ -118,7 +122,37 @@ app.post("/purchase/:id", (req, res) => {
     }
   )
     .then((result) => {
-      result: true;
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
+
+app.get("/products/:id/recommendation", (req, res) => {
+  const { id } = req.params;
+  models.Product.findOne({
+    where: {
+      id,
+    },
+  })
+    .then((product) => {
+      const type = product.type;
+      models.Product.findAll({
+        where: {
+          type,
+          id: {
+            [models.Sequelize.Op.ne]: id,
+          },
+        },
+      }).then((products) => {
+        res.send({
+          products,
+        });
+      });
     })
     .catch((error) => {
       console.error(error);
@@ -127,16 +161,15 @@ app.post("/purchase/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("유니의 쇼핑몰 서버가 돌아가고 있습니다.");
-  //models에 있는 것을 가져온다.
+  console.log("유희왕의 쇼핑몰 서버가 돌아가고 있습니다");
   models.sequelize
     .sync()
     .then(() => {
-      console.log("db 연결 성공");
+      console.log("DB 연결 성공!");
     })
     .catch((err) => {
       console.error(err);
-      console.log("db 연결 에러");
+      console.log("DB 연결 에러ㅠ");
       process.exit();
     });
 });
